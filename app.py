e9b4bbdddbb61ab21d7e16b21c4ddf7e6fee4366
Michael Zhang <mzhang00@stuy.edu>
2020-01-09 08:30:30 -0500

    Update with plain html for login and signup

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
 import os
-import sqlite3
+import urllib, json, sqlite3
+from json import loads
 app = Flask(__name__)
 DB_FILE="discobandit.db"
 
@@ -9,41 +10,23 @@ c = db.cursor()               #facilitate db ops
 
 app.secret_key = os.urandom(32)
 
-db.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT)")
-
-db.execute("CREATE TABLE IF NOT EXISTS posts (username TEXT, postID INTEGER ,verID INTEGER,title TEXT,content TEXT)")
-
-@app.route('/')
-
+@app.route("/")
 def root():
-    return "hello"
-# from flask import Flask , render_template,request, redirect, url_for, session, flash
-# import urllib, json, sqlite3
-# from json import loads
-# from utl import db_builder, db_manager
-# import os
-# app = Flask(__name__)
-# app.secret_key = os.urandom(32)
-#
-# DB_FILE = "trivia.db"
-#
-# @app.route("/")
-# def root():
-#     if 'username' in session:
-#         return redirect('/menu')
-#     return redirect('/login')
-#
+    # if 'username' in session:
+    #     return redirect('/menu')
+    return redirect('/login')
+
 # @app.route('/logout')
 # def logout():
 #     session.pop('username')
 #     session.pop('password')
 #     flash('You were successfully logged out!')
 #     return redirect('/')
-#
-# @app.route("/login")
-# def login():
-#     return render_template('login.html')
-#
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 # @app.route("/auth", methods=["POST"])
 # def auth():
 #     enteredU = request.form['username']
@@ -57,37 +40,39 @@ def root():
 #     else:
 #         flash('Wrong Credentials!', 'red')
 #         return render_template("login.html")
-#
-# @app.route("/home")
-# def home():
-#     return "LOGGED IN"
-#
-# @app.route("/signup")
-# def signup():
-#     u = urllib.request.urlopen("https://restcountries.eu/rest/v2/all")
-#     response = json.loads(u.read())
-#     allcountries=[]
-#     for country in response:
-#         allcountries.append(country['name'])
-#     return render_template("signup.html",options=allcountries)
-#
+
+@app.route("/home")
+def home():
+    return "LOGGED IN"
+
+@app.route("/signup")
+def signup():
+    # u = urllib.request.urlopen("https://restcountries.eu/rest/v2/all")
+    # response = json.loads(u.read())
+    # allcountries=[]
+    # for country in response:
+    #     allcountries.append(country['name'])
+    return render_template("signup.html")
+
 # @app.route("/signupcheck", methods=["POST"])
 # def signupcheck():
 #     username=request.form['username']
 #     password=request.form['password']
 #     confirm=request.form['confirmation']
-#     flag=request.form['flag']
-#     u = urllib.request.urlopen("https://restcountries.eu/rest/v2/all")
 #     response = json.loads(u.read())
 #     if(username=="" or password=="" or confirm==""):
 #         flash('Please fill out all fields!', 'red')
-#         return render_template("signup.html", username=username,password=password,confirm=confirm,flag=flag,options=allcountries)
+#         return render_template("signup.html", username=username,password=password)
 #     if (confirm!=password):
 #         flash('Passwords do not match!', 'red')
-#         return render_template("signup.html", username=username,password=password,confirm=confirm,flag=flag,options=allcountries)
+#         return render_template("signup.html", username=username,password=password)
 #     added = db_manager.addUser(username,password,flag)
 #     if (not added):
 #         flash('Username taken!', 'red')
-#         return render_template("signup.html", username=username,password=password,confirm=confirm,flag=flag,options=allcountries)
+#         return render_template("signup.html", username=username,password=password)
 #     flash('You have successfully created an account! Please log in!')
 #     return redirect("/login")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
